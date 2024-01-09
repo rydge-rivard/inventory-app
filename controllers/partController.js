eca96cfb9539220cd9082211ba58ac199b6dcb54
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2024-01-09 14:26:53 -0500

    Add parts list view, route, and controller.

--- controllers/partController.js
+++ controllers/partController.js
@@ -1,6 +1,19 @@
 const Part = require("../models/part");
+const Vehicle = require("../models/vehicle");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
   res.render("index", { title: "Rydge's Auto Parts" });
+});
+
+exports.parts_list = asyncHandler(async (req, res, next) => {
+  const allParts = await Part.find({}, "name vehicle description price")
+    .sort({ name: 1 })
+    .populate("vehicle")
+    .exec();
+
+  res.render("parts_list", {
+    title: "Rydge's Auto Parts",
+    parts_list: allParts,
+  });
 });
--- routes/catalog.js
+++ routes/catalog.js
@@ -6,5 +6,6 @@ const part_controller = require("../controllers/partController");
 /// PART ROUTES ///
 
 router.get("/", part_controller.index);
+router.get("/parts_list", part_controller.parts_list);
 
 module.exports = router;
